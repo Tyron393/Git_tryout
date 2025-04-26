0733811d782f30cd74fca63b2ce802c452481147
Tyron393 <39885679+Tyron393@users.noreply.github.com>
2025-04-26 13:25:30 +0200

    Added extra files

--- hello_again.py
+++ hello_again.py
@@ -1,1 +1,15 @@
-print("Hello Again")
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
+print(API_KEY)
+city = "Budapest"
+
+url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+
+response = requests.get(url)
+print(response)
+print(response.json())
